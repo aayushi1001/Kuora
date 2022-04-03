cdad03f4450ac041852efbfa355fd0e555df1ff1
AayushiAnand <AayushiAnand@fico.com>
2022-04-04 03:41:17 +0530

    fetch verification request using emailId

--- Kuora-Backend/api/routes/verification_get.js
+++ Kuora-Backend/api/routes/verification_get.js
@@ -22,4 +22,24 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.get("/:email", (req, res, next) => {
+  const email = req.params.email;
+  Verification.find({email:email})
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        code: 200,
+        message: "Verification Request Successfully found",
+        verification: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      req.status(500).json({
+        code: 500,
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
