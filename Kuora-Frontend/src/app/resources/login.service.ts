0575eea3ad6a47d6b8838c62a3666edea733063a
AayushiAnand <AayushiAnand@fico.com>
2022-04-23 23:45:26 +0530

    reload working

--- Kuora-Frontend/src/app/resources/login.service.ts
+++ Kuora-Frontend/src/app/resources/login.service.ts
@@ -1,7 +1,7 @@
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Router } from '@angular/router';
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import { userDetails } from './user-details.model';
 import jwt_decode from "jwt-decode";
 
@@ -12,7 +12,7 @@ interface LoginResponseData{
 }
 
 @Injectable()
-export class LoginService {
+export class LoginService implements OnInit{
 
     private activeUserDetails: userDetails = {
         name: 'Undefined',
@@ -24,27 +24,58 @@ export class LoginService {
         pic: 'Undefined',
     };
 
-
+    private localStorageKey = 'my_details';
     errorResponse: string = '';
 
     constructor(private http : HttpClient,
-        private router : Router) { }
+        private router : Router) {
+      let userDetails = this.getLocalStorage();
+      if(userDetails && this.activeUserDetails.email === 'Undefined'){
+        this.activeUserDetails = userDetails;
+      }
+    }
+
+    ngOnInit(){
+      let userDetails = this.getLocalStorage();
+      if(userDetails && !this.activeUserDetails){
+        this.activeUserDetails = userDetails;
+      }
+    }
 
     userLogin(userData: {email: string, password: string}){
         this.http.post<LoginResponseData>(environment.url_Api + 'login', userData)
         .subscribe(responseData => {
             if(responseData.code === 200){
                 this.activeUserDetails = jwt_decode(responseData.token);
-                console.log(this.activeUserDetails);
+                this.unsetLocalStorage();
+                this.setLocalStorage();
                 this.router.navigate(['/main-page/display-area/all']);
             } else {
                 this.errorResponse = responseData.message;
+                this.unsetLocalStorage();
             }
         })
     }
 
     getActiveUserDetails(){
         return this.activeUserDetails;
+    }
+
+    private setLocalStorage(){
+      localStorage.setItem(this.localStorageKey, JSON.stringify(this.activeUserDetails));
+    }
+
+    private getLocalStorage(){
+      let storedDetails = localStorage.getItem(this.localStorageKey);
+      let userDetails;
+      if(storedDetails) {
+        userDetails = JSON.parse(storedDetails);
+      }
+      return userDetails;
+    }
+
+    private unsetLocalStorage(){
+      localStorage.removeItem(this.localStorageKey);
     }
 
     unsetActiveUserDetails(){
@@ -55,6 +86,7 @@ export class LoginService {
       this.activeUserDetails.verified = 'Undefined';
       this.activeUserDetails.blocked = false;
       this.activeUserDetails.pic = 'Undefined';
+      this.unsetLocalStorage();
     }
 }
 
