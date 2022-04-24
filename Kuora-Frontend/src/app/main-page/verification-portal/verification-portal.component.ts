ec6199e948589ab112cf2fc0f6093a2809472383
AayushiAnand <AayushiAnand@fico.com>
2022-04-24 14:39:01 +0530

    verification portal constraint added

--- Kuora-Frontend/src/app/main-page/verification-portal/verification-portal.component.ts
+++ Kuora-Frontend/src/app/main-page/verification-portal/verification-portal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/resources/login.service';
 import { VerifyRequestService } from 'src/app/resources/verify-request.service';
+import {GetVerificationListService} from "../../resources/get-verification-list.service";
 
 @Component({
   selector: 'app-verification-portal',
@@ -11,17 +12,37 @@ import { VerifyRequestService } from 'src/app/resources/verify-request.service';
 export class VerificationPortalComponent implements OnInit {
 
   verificationForm!: FormGroup;
+  canApplyforVerification: boolean = true;
+  reason: string = '';
   constructor(private sendRequest: VerifyRequestService,
-              private loginService: LoginService) { }
+              private loginService: LoginService,
+              private verificationList: GetVerificationListService) { }
 
   ngOnInit(): void {
+
+    this.verificationList.GetList().subscribe(
+      (res) => {
+        for(let req in res.verification){
+          let email = this.loginService.getActiveUserDetails().email;
+          if(res.verification[req].email === email) {
+            this.canApplyforVerification = false;
+            this.reason = res.verification[req].request;
+            console.log(res.verification[req].request);
+            break;
+          }
+        }
+      }
+    );
+
     this.verificationForm = new FormGroup({
-      'reason': new FormControl(null, [Validators.required]),
-      'verificationDocument': new FormControl(null, [Validators.required]),
+      'reason': new FormControl({value: this.reason, disabled: this.canApplyforVerification}, [Validators.required]),
+      'verificationDocument': new FormControl({value: null, disabled: this.canApplyforVerification}, [Validators.required]),
     });
+
   }
 
   ngAfterViewInit(){
+
   }
 
   onFileChange(event: any) {
